38ba78934dac49c019364d1f161a5dd512423f36
DiibSal <dildar.sallabas@gmail.com>
2024-10-15 10:13:05 +0200

    Update slutuppgift_huvudprogram.py

--- slutuppgift_huvudprogram.py
+++ slutuppgift_huvudprogram.py
@@ -1,22 +1,8 @@
 def meny():
-#Q: Önödigt att göra for loop? Går att bara göra val2 till array och printa.
-   val2 = ["1. Starta övervakning", "2. Lista aktiv övervakning", "3. Skapa larm", "4. Visa larm", "5. Starta övervakningsläge"]
-   for x in val2:
+
+   menyval = ["1. Starta övervakning", "2. Lista aktiv övervakning", "3. Skapa larm", "4. Visa larm", "5. Starta övervakningsläge"]
+   for x in menyval:
         print(x)
-
-
-def back():
-#Q: Hur tillåter man flera inputs i en rad?
-    YN = input("Vill du återgå till startmenyn? (Y/N)")
-    if YN == "y":
-        meny()
-        huvudprogram()
-        back()
-    elif YN == "n":
-        print()
-    else:
-        print("Inget giltigt svar")
-        back()
         
         
 def huvudprogram():
@@ -28,9 +14,7 @@ def huvudprogram():
     elif val == "2":
             print("Listar övervakning")
             print()
-#Q: Repeterar första gången man kör koden
-#Importerar kod från overvakning.py för procentuell användning för CPU, minne och diskanvändning.
-            import overvakning    
+            import overvakning   #Importerar kod för procentuell användning för CPU, minne och diskanvändning.
             overvakning.usage()
             print()
             
@@ -57,5 +41,4 @@ def huvudprogram():
 while True:
     meny()
     huvudprogram()
-    back()
-    break
+    input("Tryck på valfri tangent för att återgå till start meny.")
