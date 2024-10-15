11b68e4c201033a038e35a0b3af1a1c6d6a8732c
DiibSal <dildar.sallabas@gmail.com>
2024-10-15 10:13:52 +0200

    Update overvakning.py

--- overvakning.py
+++ overvakning.py
@@ -5,10 +5,9 @@ def usage():
     ram = psutil.virtual_memory()
     print("Din RAM användning är",ram.percent,"%")
     
-    cpu = psutil.cpu_percent(interval=1, percpu=False)
+    cpu = psutil.cpu_percent(interval=0.5)
     print("Din CPU användning är ",cpu,"%")
     
     disk = psutil.disk_usage('/')
-    #Q: Visar endast värde 43?
     print("Din disk användning är ",disk.percent,"%")
 
