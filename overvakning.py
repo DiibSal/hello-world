a397ddba03fc41e459545467fa7e28836f5a38a7
DiibSal <dildar.sallabas@gmail.com>
2024-10-17 16:09:34 +0200

    Update overvakning.py

--- overvakning.py
+++ overvakning.py
@@ -1,13 +1,11 @@
 import psutil
 
 def usage():
-    
-    ram = psutil.virtual_memory()
-    print("Din RAM användning är",ram.percent,"%")
-    
-    cpu = psutil.cpu_percent(interval=0.5)
-    print("Din CPU användning är ",cpu,"%")
-    
-    disk = psutil.disk_usage('/')
-    print("Din disk användning är ",disk.percent,"%")
-
+    global ramPer, ramUse, ramTot, cpu, diskPer, diskUse, diskTot
+    cpu = psutil.cpu_percent(interval=1)
+    ramPer = psutil.virtual_memory().percent
+    ramUse = psutil.virtual_memory().used/(1000**3)
+    ramTot = psutil.virtual_memory().total/(1000**3)
+    diskPer = psutil.disk_usage("/").percent
+    diskUse = psutil.disk_usage("/").used/(1000**3)
+    diskTot = psutil.disk_usage("/").total/(1000**3)
